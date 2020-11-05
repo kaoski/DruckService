ceab04f01c78462cc37ec3fdd046e2493a6ae930
Kunal Acharya <kunalacharya74@gmail.com>
2020-11-05 13:05:01 +0530

    Initial Project Setup

---
+++ src/main/java/com/druck/druckservice/DruckserviceApplication.java
@@ -0,0 +1,13 @@
+package com.druck.druckservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruckserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DruckserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/druck/druckservice/DruckserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.druck.druckservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruckserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
